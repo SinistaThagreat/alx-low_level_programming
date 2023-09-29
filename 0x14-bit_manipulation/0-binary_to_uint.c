ef3c109a55764896ebc935d8a4f130be28124360
SinistaThagreat <backyardsquad04@gmail.com>
2023-09-29 13:28:10 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,26 @@
-#include <stdlib.h>
-#include <main.h>
+#include "main.h"
 
-unsigned int binary_to_uint(const char *b) {
-    if (b == NULL) {
-        return 0;  // NULL pointer case
-    }
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec_val = 0;
 
-    size_t length = strlen(b);
-    unsigned int result = 0;
+	if (!b)
+		return (0);
 
-    for (size_t i = 0; i < length; i++) {
-        if (b[i] != '0' && b[i] != '1') {
-            return 0;  // Non-binary character case
-        } 
-        result += (b[i] - '0') * pow(2, length - i - 1);
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-    return result;
+	return (dec_val);
 }
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,24 +1,14 @@
-#include <stdio.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    unsigned int n;
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _atoi(const char *s);
+int _putchar(char c);
+int get_endianness(void);
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
-}
+#endif
